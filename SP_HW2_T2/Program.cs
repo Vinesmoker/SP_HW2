b282caade7e9732a296bbe64b185bf6c2064dbc2
Igor Vinokurov <harryvin@bk.ru>
2024-07-05 21:41:35 +0300

    HW2_T2 Done

--- SP_HW2_T2/Program.cs
+++ SP_HW2_T2/Program.cs
@@ -1,1 +1,45 @@
-﻿
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using static System.Console;
+
+class Program
+{
+    static void Main()
+    {
+        WriteLine("Введите имя исполняемого файла дочернего процесса:");
+        string? fileName = ReadLine();
+        ProcessStartInfo startInfo = new()
+        {
+            FileName = fileName,
+            Arguments = @"C:\\Users\\User\\Desktop\\2.txt"
+        };
+        using Process? proc = Process.Start(startInfo);
+        if (proc == null)
+        {
+            WriteLine("Не удалось запустить дочерний процесс.");
+            return;
+        }
+        WriteLine("Выберите действие:");
+        WriteLine("1 - Ожидать завершения дочернего процесса");
+        WriteLine("2 - Принудительно завершить дочерний процесс");
+        string? choice = ReadLine();
+        switch (choice)
+        {
+            case "1":
+                proc.WaitForExit();
+                WriteLine($"Дочерний процесс завершен с кодом {proc.ExitCode}.");
+                break;
+            case "2":
+                if (!proc.HasExited)
+                {
+                    proc.Kill();
+                    WriteLine("Дочерний процесс был принудительно завершен.");
+                }
+                break;
+            default:
+                WriteLine("Неверный выбор.");
+                break;
+        }
+    }
+}
