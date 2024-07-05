7f3c496443b77fb6c3cebe7962f0842174850ce8
Igor Vinokurov <harryvin@bk.ru>
2024-07-05 23:09:59 +0300

    HW2_T3 Done

---
+++ SP_HW2_T3_Part1/Program.cs
@@ -0,0 +1,49 @@
+﻿#nullable enable
+using System;
+using System.Diagnostics;
+using static System.Console;
+
+class ParentProgram
+{
+    static void Main()
+    {
+        bool cont = true;
+        while (cont)
+        {
+            WriteLine("Введите первое число:");
+            string? num1 = ReadLine();
+            WriteLine("Введите действие (+, -, *, /):");
+            string? act = ReadLine();
+            WriteLine("Введите второе число:");
+            string? num2 = ReadLine();
+            if (num1 != null && act != null && num2 != null)
+            {
+                ProcessStartInfo start = new()
+                {
+                    FileName = @"C:\\Users\\User\\source\\repos\\SP_HW2\\SP_HW2_T3_Part2_Child\\bin\\Debug\\net8.0\\SP_HW2_T3_Part2_Child.exe",
+                    Arguments = $"{num1} {act} {num2}"
+                };
+
+                using Process? childProc = Process.Start(start);
+                if (childProc != null)
+                {
+                    childProc.WaitForExit();
+                }
+                else
+                {
+                    WriteLine("Не удалось запустить дочерний процесс.");
+                }
+            }
+            else
+            {
+                WriteLine("Одно из введенных значений было пустым. Пожалуйста, попробуйте снова.");
+            }
+            WriteLine("Нажмите (Y) для повтора или любую клавишу для выхода.");
+            string? choice = ReadLine()?.ToLower();
+            if (choice != "y" && choice != "н" && choice != "да" && choice != "yes")
+            {
+                cont = false;
+            }
+        }
+    }
+}
---
+++ SP_HW2_T3_Part2_Child/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using static System.Console;
+
+class ChildProgram
+{
+    static void Main(string[] args)
+    {
+        if (args.Length != 3)
+        {
+            WriteLine("Некорректное количество аргументов.");
+            return;
+        }
+        if (!double.TryParse(args[0], out double num1) || !double.TryParse(args[2], out double num2))
+        {
+            WriteLine("Некорректный ввод чисел.");
+            return;
+        }
+
+        string? action = args[1];
+        double result;
+
+        switch (action)
+        {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                if (num2 == 0)
+                {
+                    WriteLine("На ноль делить нельзя.");
+                    return;
+                }
+                result = num1 / num2;
+                break;
+            default:
+                WriteLine("Некорректное действие.");
+                return;
+        }
+        WriteLine($"Результат: {result}");
+    }
+}
